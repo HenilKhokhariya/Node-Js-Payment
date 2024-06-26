8b6236d4054328720de8b0453fb7e626e393f634
HenilKhokhariya <henilkhokhariya27@gmail.com>
2024-06-26 15:13:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
+
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "./")));
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, ".index.html"));
-});
+
 const fixedAmount = "1.00";
-app.post("/pay", (req, res) => {
+app.post("/", (req, res) => {
   const payeeVPA = "SBIPMOPAD.02SOB0000021160-YM542163@sbipay";
   const payeeName = "henil";
   const currency = "INR";
