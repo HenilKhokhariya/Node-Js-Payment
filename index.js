04c3ef660908da6f6363ba5fc95efd438ebcd19f
HenilKhokhariya <henilkhokhariya27@gmail.com>
2024-06-26 13:30:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const { amount } = 1;
-  const payeeVPA = "sanketbholabhainarol.65004758@hdfcbank";
-  const deepLink = `phonepe://pay?pa=${payeeVPA}&amount=${amount}&cu=IND&callbackUrl=https://node-js-payment.onrender.com/payment-response`;
+  const payeeVPA = "SBIPMOPAD.02SOB0000021160-YM542163@sbipay";
+  const payeeName = "He";
+  const deepLink = `phonepe://pay?pa=${payeeVPA}&pn=${payeeName}&amount=${amount}&cu=IND&callbackUrl=https://node-js-payment.onrender.com/payment-response`;
   res.send(`<html>
     <body>
       <a href="${deepLink}">Pay Now</a>
