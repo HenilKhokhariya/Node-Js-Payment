8aba6e9460b421a80501607deea059aacd4ad72c
Henil Khokhariya <henilkhokhariya27@gmail.com>
2024-06-26 13:01:58 +0530

    new

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Payment App Integration');
+app.get("/", (req, res) => {
+  res.send("Payment App Integration");
 });
 
-app.post('/initiate-payment', (req, res) => {
+app.post("/initiate-payment", (req, res) => {
   const { amount } = req.body;
-  const deepLink = `paymentapp://pay?amount=${amount}&callbackUrl=http://yourserver.com/payment-response`;
+  const payeeVPA = "SBIPMOPAD.02SOB0000021160-YM542163@sbipay";
+  const deepLink = `paymentapp://pay?pa=${payeeVPA}&amount=${amount}&cu=IND&callbackUrl=https://node-js-payment.onrender.com/payment-response`;
   res.send(`<html>
     <body>
       <a href="${deepLink}">Pay Now</a>
@@ -20,11 +21,11 @@ app.post('/initiate-payment', (req, res) => {
   </html>`);
 });
 
-app.post('/payment-response', (req, res) => {
+app.post("/payment-response", (req, res) => {
   const { status, transactionId } = req.body;
   // Process the payment response
   console.log(`Payment Status: ${status}, Transaction ID: ${transactionId}`);
-  res.send(`Payment ${status ? 'Successful' : 'Failed'}!`);
+  res.send(`Payment ${status ? "Successful" : "Failed"}!`);
 });
 
 app.listen(port, () => {
